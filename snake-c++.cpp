dec0f9191307f31838e1f7e0bbe74e74587543e3
BonLacorte <bonbonlacorte123@gmail.com>
2021-08-02 21:29:15 +0800

    changes 1

--- snake-c++.cpp
+++ snake-c++.cpp
@@ -2,12 +2,47 @@
 
 using namespace std;
 
-void Setup() {
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
 
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 }
 
 void Draw() {
+    system("cls");
+    for ( int i = 0; i < width + 2; i++ )
+        cout << "#";
+    
+    cout << endl;
 
+    for ( int i = 0; i < height; i++){
+        for ( int j = 0; j < width; j++){
+            if ( j == 0){
+                cout << "*" << endl;
+            } else if ( j == width - 1) {
+                cout << "*" << endl;
+            } else {
+                cout << " ";
+            }
+            cout << endl;
+        }
+    }
+
+
+    for ( int i = 0; i < width + 2; i++ )
+        cout << "#";
+    cout << endl;
 }
 
 void Input() {
@@ -19,6 +54,14 @@ void Logic(){
 }
 
 int main() {
+    
+    Setup();
 
+    while ( !gameOver ) {
+        Draw();
+        Input();
+        Logic();
+        //Sleep(10); sleep(10);
+    }
     return 0;
 }
